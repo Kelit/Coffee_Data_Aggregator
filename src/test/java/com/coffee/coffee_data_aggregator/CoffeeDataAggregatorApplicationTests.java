b8d1482367c4aab5f7f7ba382188cd0c4c2b91cb
Denis <dr.kelit@gmail.com>
2021-04-19 18:54:46 +0700

    init

---
+++ src/test/java/com/coffee/coffee_data_aggregator/CoffeeDataAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.coffee_data_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeDataAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
