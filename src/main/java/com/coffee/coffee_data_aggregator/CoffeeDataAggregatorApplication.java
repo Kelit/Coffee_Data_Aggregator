6344a7319fcc2aceb3e66f6dfb64293ec54901e2
Denis <dr.kelit@gmail.com>
2021-04-19 19:14:53 +0700

    Controllers

--- src/main/java/com/coffee/coffee_data_aggregator/CoffeeDataAggregatorApplication.java
+++ src/main/java/com/coffee/coffee_data_aggregator/CoffeeDataAggregatorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoffeeDataAggregatorApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CoffeeDataAggregatorApplication.class, args);
     }
-
 }
