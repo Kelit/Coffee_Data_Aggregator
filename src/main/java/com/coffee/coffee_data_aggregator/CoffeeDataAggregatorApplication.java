85c83bff5b0b29d564c16bac13e6340f06a38154
denis <d.kataev@kbhmb.ru>
2021-04-20 09:43:23 +0700

    add resource handlers

--- src/main/java/com/coffee/coffee_data_aggregator/CoffeeDataAggregatorApplication.java
+++ src/main/java/com/coffee/coffee_data_aggregator/CoffeeDataAggregatorApplication.java
@@ -2,10 +2,17 @@ package com.coffee.coffee_data_aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class CoffeeDataAggregatorApplication {
+public class CoffeeDataAggregatorApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(CoffeeDataAggregatorApplication.class, args);
     }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
 }
+
